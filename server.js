ae51c3b0d5d48a8dc4a983e2cb24682bb5526a79
Karl Beecken <karl@beecken.berlin>
2020-08-25 22:31:31 +0200

    rewrite stuff as needed

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
-var express = require('express');
-var passport = require('passport');
-var Strategy = require('passport-twitter').Strategy;
+var express = require("express");
+var passport = require("passport");
+var Strategy = require("passport-twitter").Strategy;
 
-var trustProxy = false;
-if (process.env.DYNO) {
-  // Apps on heroku are behind a trusted proxy
-  trustProxy = true;
-}
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
+const adapter = new FileSync("tokens.json");
+const db = low(adapter);
+db.defaults({ accounts: [] }).write();
 
 // Configure the Twitter strategy for use by Passport.
 //
@@ -18,41 +18,55 @@ if (process.env.DYNO) {
 // user's behalf, along with the user's profile.  The function must invoke `cb`
 // with a user object, which will be set at `req.user` in route handlers after
 // authentication.
-passport.use(new Strategy({
-    consumerKey: process.env['CONSUMER_KEY'],
-    consumerSecret: process.env['CONSUMER_SECRET'],
-    callbackURL: '/auth/twitter/callback',
-    proxy: trustProxy
-  },
-  function(token, tokenSecret, profile, cb) {
-    // In this example, the user's Twitter profile is supplied as the user
-    // record.  In a production-quality application, the Twitter profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
+passport.use(
+  new Strategy(
+    {
+      consumerKey: process.env["CONSUMER_KEY"],
+      consumerSecret: process.env["CONSUMER_SECRET"],
+      callbackURL: "/auth/twitter/callback",
+    },
+    function (token, tokenSecret, profile, cb) {
+      // In this example, the user's Twitter profile is supplied as the user
+      // record.  In a production-quality application, the Twitter profile should
+      // be associated with a user record in the application's database, which
+      // allows for account linking and authentication with other identity
+      // providers.
 
-    var Twit = require('twit')
+      // var Twit = require("twit");
 
-    var T = new Twit({
-      consumer_key:         process.env['CONSUMER_KEY'], //get this from developer.twitter.com where your app info is
-      consumer_secret:      process.env['CONSUMER_SECRET'], //get this from developer.twitter.com where your app info is
-      access_token:         token,
-      access_token_secret:  tokenSecret,
-      timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-      strictSSL:            true,     // optional - requires SSL certificates to be valid.
-    })
-  
-    //
-    //  tweet 'hello world!'
-    //
-    // T.post('statuses/update', { status: 'hello world!' }, function(err, 
-    // data, response) {
-    //   console.log(data)
-    // })
+      // var T = new Twit({
+      //   consumer_key: process.env["CONSUMER_KEY"], //get this from developer.twitter.com where your app info is
+      //   consumer_secret: process.env["CONSUMER_SECRET"], //get this from developer.twitter.com where your app info is
+      //   access_token: token,
+      //   access_token_secret: tokenSecret,
+      //   timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+      //   strictSSL: true, // optional - requires SSL certificates to be valid.
+      // });
 
-    return cb(null, profile);
-  }));
+      db.get("accounts")
+        .push({ id: profile.id, user: profile.username, token, tokenSecret })
+        .write();
 
+      const newAccounts = db.get("accounts").uniqBy("user").value();
+
+      db.set("accounts", newAccounts).write();
+
+      console.log(profile.username);
+      console.log(token);
+      console.log(tokenSecret);
+
+      //
+      //  tweet 'hello world!'
+      //
+      // T.post('statuses/update', { status: 'hello world!' }, function(err,
+      // data, response) {
+      //   console.log(data)
+      // })
+
+      return cb(null, profile);
+    }
+  )
+);
 
 // Configure Passport authenticated session persistence.
 //
@@ -63,70 +77,70 @@ passport.use(new Strategy({
 // from the database when deserializing.  However, due to the fact that this
 // example does not have a database, the complete Twitter profile is serialized
 // and deserialized.
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
   cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
+passport.deserializeUser(function (obj, cb) {
   cb(null, obj);
 });
-
 
 // Create a new Express application.
 var app = express();
 
 // Configure view engine to render EJS templates.
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
-app.use(require('morgan')('combined'));
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(require("morgan")("combined"));
+app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // Define routes.
-app.get('/',
-  function(req, res) {
-    res.render('home', { user: req.user });
+app.get("/", function (req, res) {
+  res.render("home", { user: req.user });
+});
+
+app.get("/login", function (req, res) {
+  console.log("Headers:");
+  console.log(req.headers);
+  res.render("login");
+});
+
+app.get("/login/twitter", passport.authenticate("twitter"));
+
+app.get(
+  "/auth/twitter/callback",
+  passport.authenticate("twitter", { failureRedirect: "/login" }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
+app.get("/profile", require("connect-ensure-login").ensureLoggedIn(), function (
+  req,
+  res
+) {
+  res.render("profile", { user: req.user });
+});
+
+app.get("/logout", function (req, res) {
+  req.session.destroy(function (err) {
+    res.redirect("/");
   });
+});
 
-app.get('/login',
-  function(req, res){
-    console.log('ENV');
-    console.log(process.env);
-    console.log('Headers:');
-    console.log(req.headers)
-    res.render('login');
-  });
-
-app.get('/login/twitter',
-  passport.authenticate('twitter'));
-
-app.get('/auth/twitter/callback',
-  passport.authenticate('twitter', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
-
-app.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res){
-    console.log(req.user)
-    res.render('profile', { user: req.user });
-  });
-
-app.get('/logout',
-  function(req, res){
-    req.session.destroy(function (err) {
-      res.redirect('/');
-    });
-  });
-
-app.listen(process.env['PORT'] || 8080);
+app.listen(process.env["PORT"] || 8080);
